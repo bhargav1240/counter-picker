0db51afe054490032e8560b1a7a7a5cebb97960e
bhargav1240 <bhavu92@gmail.com>
2020-05-22 19:49:55 +0530

    create hero and counter model

---
+++ database/factories/HeroFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Hero;
+use Faker\Generator as Faker;
+
+$factory->define(Hero::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_05_22_140911_create_counters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('hero_id')->unsigned()->index();
+            $table->integer('counterd_by');
+            $table->float('score');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counters');
+    }
+}
